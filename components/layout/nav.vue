4c644e75f1d3f35d6ac82689ad533e8c7a24fd27
youngkyunjung <younginpiniti@gmail.com>
2024-05-10 07:34:25 +0900

    주문 card 수정

--- components/layout/nav.vue
+++ components/layout/nav.vue
@@ -4,7 +4,7 @@ const _works = works();
 <template>
   <div class="p-2 flex flex-col gap-2">
     <NuxtLink to="/work.post">
-      <Card class="p-2">+ 주문</Card>
+      <Card class="p-2 text-sm">+ 주문 등록</Card>
     </NuxtLink>
     <NavWork v-for="_work in _works" :_work="_work" />
   </div>
--- components/nav/work.vue
+++ components/nav/work.vue
@@ -6,28 +6,41 @@ const props = defineProps<{
 function onClickWork() {
   work().value = work().value = props._work;
 }
+
+const 남은일자 = computed(() => {
+  if (props._work.deadline) {
+    let deadline = new Date(props._work.deadline);
+    let today = new Date();
+    let diff = deadline.getTime() - today.getTime();
+    let days = Math.ceil(diff / (1000 * 60 * 60 * 24));
+    return days;
+  }
+  return 0;
+});
 </script>
 <template>
   <Card @click="onClickWork">
     <div class="p-2 text-xs flex flex-col gap-1" v-if="_work">
-      <div class="flex gap-2">
+      <div class="flex justify-between">
         <Badge>
           {{ WORK_ITEM[_work?.work_item as keyof typeof WORK_ITEM] }}
         </Badge>
-        <div class="text-sm font-bold">{{ _work?.work_name }}</div>
+        <div>{{ 남은일자 }}일 남음</div>
       </div>
-      <div class="flex gap-4">
-        <div class="flex gap-1">
+      <div class="text-sm font-bold">{{ _work?.work_name }}</div>
+      <div class="flex gap-3">
+        <div class="flex flex-col">
           <div class="text-neutral-400">고객</div>
           <div>{{ _work?.customer_name }}</div>
         </div>
-        <div class="flex gap-1">
+        <div class="flex flex-col">
           <div class="text-neutral-400">연락처</div>
           <div>{{ _work?.customer_phone }}</div>
         </div>
       </div>
       <div class="text-neutral-400">
-        {{ _work?.work_registration_date }} ~ {{ _work?.deadline }}
+        {{ _work?.work_registration_date }} ~
+        <span class="text-red-400">{{ _work?.deadline }}</span>
       </div>
     </div>
   </Card>
