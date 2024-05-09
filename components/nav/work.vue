6d51edafcc8cd150dfba0c3b79e74534a74ea22c
youngkyunjung <younginpiniti@gmail.com>
2024-05-10 07:27:16 +0900

    주문 card 수정

--- components/nav/work.vue
+++ components/nav/work.vue
@@ -4,16 +4,31 @@ const props = defineProps<{
 }>();
 
 function onClickWork() {
-  work().value = props._work;
+  work().value = work().value = props._work;
 }
 </script>
 <template>
   <Card @click="onClickWork">
-    <div class="p-2" v-if="_work">
-      <div class="font-bold">{{ _work?.customer_name }}</div>
-      <div>{{ _work?.work_item }}</div>
-      <div class="font-bold">상태 : 주문</div>
-      <div>{{ _work?.work_registration_date }}</div>
+    <div class="p-2 text-xs flex flex-col gap-1" v-if="_work">
+      <div class="flex gap-2">
+        <Badge>
+          {{ WORK_ITEM[_work?.work_item as keyof typeof WORK_ITEM] }}
+        </Badge>
+        <div class="text-sm font-bold">{{ _work?.work_name }}</div>
+      </div>
+      <div class="flex gap-4">
+        <div class="flex gap-1">
+          <div class="text-neutral-400">고객</div>
+          <div>{{ _work?.customer_name }}</div>
+        </div>
+        <div class="flex gap-1">
+          <div class="text-neutral-400">연락처</div>
+          <div>{{ _work?.customer_phone }}</div>
+        </div>
+      </div>
+      <div class="text-neutral-400">
+        {{ _work?.work_registration_date }} ~ {{ _work?.deadline }}
+      </div>
     </div>
   </Card>
 </template>
--- composables/useWork.ts
+++ composables/useWork.ts
@@ -15,6 +15,20 @@ export interface IWork {
   work_width: number;
 }
 
+export const WORK_ITEM = {
+  billboard: "현수막",
+  banner: "배너",
+  car_magnet: "차량자석",
+  roll_poster: "롤포스터",
+  shoulder_strap: "어깨띠",
+  name_tag: "명찰",
+  print: "인쇄",
+  synthetic_paper: "합성지",
+  pet: "펫트",
+  transparent_film: "투명필름",
+  others: "기타",
+};
+
 export const works = () => useState<IWork[]>("works");
 export const work = () => useState<IWork>("work");
 
