5eca6fa8f2ab64d6074b0640d47bedd945214822
inpiniti <inpiniti@gmail.com>
2024-05-14 13:36:02 +0900

    제작 화면 수정

--- pages/production.vue
+++ pages/production.vue
@@ -1,1 +1,13 @@
-<template>production</template>
+<template>
+  <div class="flex flex-col gap-4 p-4">
+    <div>
+      <h1 class="text-lg font-bold">제작</h1>
+      <p class="text-sm text-muted-foreground">제작중인 사진 등록 및 상태 변경을 할 수 있습니다.</p>
+    </div>
+    <div class="flex gap-4">
+      <Card class="p-4">진행전</Card>
+      <Card class="p-4">제작중</Card>
+      <Card class="p-4">제작완료</Card>
+    </div>
+  </div>
+</template>
