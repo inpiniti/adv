8c3c373c2c7bfd10df085e8213b453beb170fc54
inpiniti <inpiniti@gmail.com>
2024-05-16 09:27:39 +0900

    모바일 작업

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,10 +2,28 @@
 import { ref } from "vue";
 
 const show = ref(false);
+const showDetail = ref(false); // 상세를 표시할지 여부를 결정하는 변수
+const isMobile = ref(false); // 화면이 모바일 크기인지 결정하는 변수
+
+onMounted(() => {
+  // Component is mounted and window is available
+  isMobile.value = window.innerWidth <= 640;
+
+  // 화면 크기가 변경될 때마다 isMobile 변수를 업데이트
+  window.addEventListener("resize", () => {
+    console.log("resize");
+    console.log(window.innerWidth);
+    isMobile.value = window.innerWidth <= 640;
+    console.log(isMobile.value);
+  });
+});
+
+// 모바일 화면에서만 showDetail 변수를 사용하도록 computed 속성을 생성
+const showDetailOnMobile = computed(() => isMobile.value && showDetail.value);
 </script>
 <template>
   <div class="w-screen h-screen overflow-hidden flex">
-    <div class="shrink-0 w-56 flex flex-col">
+    <div class="lg:shrink-0 lg:w-56 w-full flex flex-col" v-show="!showDetailOnMobile">
       <div class="shrink-0">
         <LayoutHeader />
       </div>
@@ -14,8 +32,8 @@ const show = ref(false);
         <LayoutNav />
       </ScrollArea>
     </div>
-    <Separator orientation="vertical" />
-    <div class="grow-[0] w-full flex flex-col">
+    <Separator class="hidden lg:block" orientation="vertical" />
+    <div class="lg:grow-[0] lg:w-full lg:flex flex-col" v-show="showDetailOnMobile">
       <div class="shrink-0">
         <LayoutSection />
       </div>
